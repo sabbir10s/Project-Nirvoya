56bb6fcac7ba460afd550430db9e34dba47dcd07
sabbir10s <sabbirahmed1023@gmail.com>
2023-01-28 21:06:02 +0600

    product details middel part done

--- src/Pages/ProductDetails/MiddleSection.js
+++ src/Pages/ProductDetails/MiddleSection.js
@@ -25,6 +25,15 @@ const MiddleSection = () => {
                         </ul>
                     </div>
                 </div>
+
+                <div className='px-[48px] pb-[39px] mt-[25px]'>
+                    <h3 className='font-semibold text-[#333333] text-[24px]'>Powerful intelligence for perfection</h3>
+                    <p className='text-[18px] mt-[18px] text-[#333333]'>
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? <br /> <br />
+
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+                    </p>
+                </div>
             </div>
         </div>
     );
