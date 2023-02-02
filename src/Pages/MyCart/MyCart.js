b88b69408a3802e5c1d1f8193b10bda8f0516428
sabbir10s <sabbirahmed1023@gmail.com>
2023-02-02 13:58:12 +0600

    cart page responsive done

--- src/Pages/MyCart/MyCart.js
+++ src/Pages/MyCart/MyCart.js
@@ -7,9 +7,9 @@ import '../../Shared/checkBoxStyle.css'
 const MyCart = () => {
     const [products] = useProducts([])
     return (
-        <div className='px-[20px] lg:px-[32px] mt-[46px] mb-[50px] grid grid-cols-4 gap-[28px]'>
-            <div className=' col-span-3'>
-                <div className='bg-white py-[32px] pl-[27px] pr-[48px] '>
+        <div className='px-[20px] lg:px-[32px] mt-[46px] mb-[50px] grid lg:grid-cols-4 gap-[28px]'>
+            <div className=' col-span-1 lg:col-span-3'>
+                <div className='bg-white px-[10px] py-[20px] lg:py-[32px] lg:pl-[27px] lg:pr-[48px] mb-[30px]'>
                     <div className='flex items-center justify-between '>
                         <div>
                             <label class="main">Select All
@@ -26,10 +26,10 @@ const MyCart = () => {
                 <div>
 
                     {
-                        products.slice(0, 4).map(product => <div className='bg-white py-[32px] pl-[27px] pr-[48px] mt-[17px]'>
-                            <div className='flex items-start justify-between'>
-                                <div className='flex items-start gap-[22px]'>
-                                    <div className='flex items-center gap-[38px]'>
+                        products.slice(0, 4).map(product => <div className='bg-white p-[10px] lg:py-[32px] lg:pl-[27px] lg:pr-[48px] mt-[25px] lg:mt-[17px]'>
+                            <div className='flex flex-col lg:flex-row items-start justify-between'>
+                                <div className='flex flex-col lg:flex-row items-start gap-[22px]'>
+                                    <div className='flex items-start lg:items-center gap-[10px] lg:gap-[38px]'>
                                         <label className="main">
                                             <input type="checkbox" />
                                             <span className="box"></span>
@@ -38,12 +38,12 @@ const MyCart = () => {
                                             <img src={product.image} alt="" />
                                         </div>
                                     </div>
-                                    <div className='flex flex-col gap-[16px]'>
-                                        <h2 className='text-[18px] font-semibold'>{product.title}</h2>
+                                    <div className='flex flex-col gap-[10px] lg:gap-[16px]'>
+                                        <h2 className='lg:text-[18px] font-semibold'>{product.title}</h2>
                                         <p className='text-secondary text-[20px] font-semibold'>{product.price}</p>
                                     </div>
                                 </div>
-                                <div className='flex flex-col gap-[47px] '>
+                                <div className='flex flex-col gap-[20px] lg:gap-[47px] '>
                                     <p className='text-[18px] text-[#4D4D4D]'>SKU 12314124124</p>
                                     <div className='flex items-center gap-[20px]'>
                                         <div className='w-[120px] h-[56px] flex items-center justify-center gap-[16px] border-[1px] border-[#D8D8D8] rounded-[5px] shadow-[0_1px_5px_4px_rgba(0,0,0,0.05)]'>
@@ -81,7 +81,7 @@ const MyCart = () => {
                     <p className='text-[28px] text-secondary font-bold flex items-center '><TbCurrencyTaka />1,952.66</p>
                 </div>
 
-                <Link to='/checkOut' ><button className='uppercase text-white text-[20px] font-bold bg-secondary w-full py-[13px] rounded-[5px] '>proceed to checkout</button></Link>
+                <Link to='/checkOut' ><button className='uppercase text-white text-[18px] font-semibold bg-secondary w-full py-[13px] rounded-[5px] '>proceed to checkout</button></Link>
             </div>
 
         </div >
